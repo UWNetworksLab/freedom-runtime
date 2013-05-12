75cc9ade6b92ec137a7a52752ce2b73b3cda9f2c
Will Scott <willscott@gmail.com>
2013-05-11 23:55:03 -0700

    visible proof of injection

--- js/portal.js
+++ js/portal.js
@@ -1,4 +1,8 @@
 (function() {
+  console.log('injection executed.');
+  var test = document.createElement('pre');
+  test.innerHTML = 'Injection proof.';
+  document.body.appendChild(test);
   window.addEventListener('message', function(m) {
     console.log('injection got message');
   });

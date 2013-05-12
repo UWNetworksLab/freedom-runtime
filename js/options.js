c31c7a9b9ed243fc19a348be0cd63debf247fefd
Will Scott <willscott@gmail.com>
2013-05-11 21:05:43 -0700

    Investigating what an intermediary page can do

--- js/options.js
+++ js/options.js
@@ -1,1 +1,38 @@
-var x = new ServiceAdvertiser({});
+window.addEventListener('load', function() {
+  var webview = document.getElementById('portal');
+  webview.addEventListener('loadstop', function() {
+    setupWebview();
+  });
+}, true);
+
+var webviewSetup = false;
+var setupWebview = function() {
+  if (webviewSetup) {
+    return;
+  } else {
+    webviewSetup = true;
+  }
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/js/portal.js', true);
+  xhr.responseType = 'text';
+  xhr.onload = function(e) {
+    injectWebview(this.response);
+  };
+  xhr.send();
+};
+
+var injectWebview = function(code) {
+  var webview = document.getElementById('portal');
+  webview.executeScript({
+    'code': code
+  }, function() {
+    webview.contentWindow.postMessage({
+      'action': 'registerApp'
+    }, '*');
+  });
+};
+
+window.addEventListener('message', function(m) {
+  console.log(m.data);
+}, true);
---
+++ js/portal.js
@@ -0,0 +1,1 @@
+console.log('testing 2');

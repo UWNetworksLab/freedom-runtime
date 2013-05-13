3342e04cba355fa633c4bf293919888dd2d02f52
Will Scott <willscott@gmail.com>
2013-05-13 11:02:27 -0700

    move freedom-chrome to an extension, show injection Proof of concept

--- background.js
+++ background.js
@@ -1,14 +1,41 @@
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  runApp(launchData);
-});
+function registerRules() {
+  var injection = chrome.extension.getURL("js/inject.js");
+  var rule = {
+    priority: 100,
+    conditions: [
+      new chrome.declarativeWebRequest.RequestMatcher({
+        url: {hostEquals: '127.3733366'}
+      }),
+      new chrome.declarativeWebRequest.RequestMatcher({
+        url: {hostEquals: '127.56.247.118'}
+      })
+    ],
+    actions: [
+      new chrome.declarativeWebRequest.RedirectRequest({
+        redirectUrl: injection
+      })
+    ]
+  };
+  
+  var callback = function() {
+    if (chrome.runtime.lastError) {
+      console.error('Error adding rules: ' + chrome.runtime.lastError);
+    } else {
+      console.info('Rules successfully installed');
+    }
+  };
 
-chrome.app.runtime.onRestarted.addListener(function(launchData) {
-  runApp(launchData);
-});
+  chrome.declarativeWebRequest.onRequest.addRules([rule], callback);
+}
 
-function runApp(launchData) {
-  console.log(launchData);
-  chrome.app.window.create('options.html', {
-    id: "Options",
+function setup() {
+  chrome.declarativeWebRequest.onRequest.removeRules(null, function() {
+    if (chrome.runtime.lastError) {
+      console.error('Error clearing rules: ' + chrome.runtime.lastError);
+    } else {
+      registerRules();
+    }
   });
 }
+
+chrome.runtime.onInstalled.addListener(setup);

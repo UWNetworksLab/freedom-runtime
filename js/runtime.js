1e7feaa0a89be9a36310acb25b52731aa74051e6
Will Scott <willscott@gmail.com>
2013-09-08 20:25:41 +0900

    update runtime to allow dynamic creation of apps

--- js/runtime.js
+++ js/runtime.js
@@ -5,7 +5,16 @@
  * @constructor
  * @private
  */
-var Runtime_chrome = function(channel) {
-  this.app = channel;
+var Runtime_chrome = function(app) {
+  this.app = app;
 };
 
+Runtime_chrome.prototype.createApp = function(manifest, proxy, contination) {
+  this.app.emit(this.app.controlChannel, {
+    type: 'Runtime App @ custom-' + proxy[0] + '.' + proxy[1],
+    name: 'custom-' + proxy[0] + proxy[1],
+    request: 'port',
+    service: 'App',
+    args: manifest
+  });
+};

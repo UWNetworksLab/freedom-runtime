7a54e88ef54a7dc3651772bb5d6960310621d5da
Will Scott <willscott@gmail.com>
2013-09-24 08:04:05 +0900

    benchmark running in series @30 p/s

--- benchmark/main.js
+++ benchmark/main.js
@@ -3,24 +3,38 @@ var core = freedom.core();
 var channels = [];
 var msgs = 0;
 var start = (new Date()).valueOf();
+var stop = false;
 
-var onPage = function(page) {
+freedom.on('stop', function() {
+  stop = true;
+});
+
+var onPage = function(channel, page) {
   msgs += 1;
+  if (!stop) {
+    work(channel);
+  } else {
+    stat();
+  }
+  if (msgs % 100 === 0) {
+    setTimeout(stat, 0);
+  }
+};
+
+var stat = function() {
   var date = ((new Date()).valueOf() - start) / 1000;
   var rate = msgs / date;
   freedom.emit('rate', rate);
 };
 
 var work = function(channel) {
-  
+  channel.emit('render', [10, 'test message']);
 };
 
 core.createChannel().done(function(channel) {
   runtime.createApp("task.json", channel.identifier);
   channel.channel.done(function(chan) {
+    chan.on('page', onPage.bind({}, chan));
     work(chan);
-    chan.on('page', onPage);
-    chan.emit('render', [10, 'test message']);
   });
 });
-

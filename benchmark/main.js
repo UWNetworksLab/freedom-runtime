b95a1dd46ba574dc79962cfad736bb9ee45c4ea9
Will Scott <willscott@gmail.com>
2013-09-24 09:54:08 +0900

    allow selection of #workers

--- benchmark/main.js
+++ benchmark/main.js
@@ -1,8 +1,6 @@
 var runtime = freedom['core.runtime']();
 var core = freedom.core();
-var channels = [];
 var msgs = 0;
-var start = (new Date()).valueOf();
 var stop = false;
 
 freedom.on('stop', function() {
@@ -13,28 +11,25 @@ var onPage = function(channel, page) {
   msgs += 1;
   if (!stop) {
     work(channel);
-  } else {
-    stat();
-  }
-  if (msgs % 100 === 0) {
-    setTimeout(stat, 0);
   }
 };
 
 var stat = function() {
-  var date = ((new Date()).valueOf() - start) / 1000;
-  var rate = msgs / date;
-  freedom.emit('rate', rate);
+  freedom.emit('rate', msgs);
+  msgs = 0;
 };
+setInterval(stat, 1000);
 
 var work = function(channel) {
   channel.emit('render', [10, 'test message']);
 };
 
-core.createChannel().done(function(channel) {
-  runtime.createApp("task.json", channel.identifier);
-  channel.channel.done(function(chan) {
-    chan.on('page', onPage.bind({}, chan));
-    work(chan);
+freedom.on('launch', function() {
+  core.createChannel().done(function(channel) {
+    runtime.createApp("task.json", channel.identifier);
+    channel.channel.done(function(chan) {
+      chan.on('page', onPage.bind({}, chan));
+      work(chan);
+    });
   });
 });
